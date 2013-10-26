c09d2d52f3a29b134d2585c234e750105c06fda5
Michal Papis <mpapis@gmail.com>
2013-10-27 00:27:29 +0200

    improve comments in tests

--- test/script_rvm-auto_comment_test.sh
+++ test/script_rvm-auto_comment_test.sh
@@ -2,14 +2,18 @@
 
 script=script/rvm-auto.sh
 
+: errors
+
 ## no ruby version given
-$script                # status=101
+$script                               # status=101
 
 ## wrong ruby given
-$script no_ruby_found  # status=103
+$script no_ruby_found                 # status=103
 
 ## no rvm found
 rvm_path=random $script no_ruby_found # status=102
+
+: proper paths
 
 ## use ruby from current dir - Gemfile is set to 1.8.7
 $script . ruby -v # match=/1.8.7/
